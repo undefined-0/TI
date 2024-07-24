b769834cf56ec5bca953c5aafa2c2d170a0560b6
undefined-0 <3047423259@qq.com>
2024-07-24 10:15:27 +0800

    新增按键亮灯工程

---
+++ LED_flash/main.c
@@ -0,0 +1,15 @@
+#include <msp430.h>
+
+int main(void)
+{
+    volatile unsigned int i;
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+    P1DIR |= 0x01;                            // 初始化LED1所在IO口P1.0为输出模式
+
+    while(1)
+    {
+        P1OUT ^= 0x01;                        // P1.0高低电平状态翻转
+
+        for (i=1000; i>0; i--);
+  }
+}
